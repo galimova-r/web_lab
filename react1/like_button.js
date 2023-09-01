8fc13b757f559acd16233173c3e763135c77a6e1
galimova-r <113368059+galimova-r@users.noreply.github.com>
2023-09-01 22:03:50 +0300

    Add files via upload

---
+++ react1/like_button.js
@@ -0,0 +1,28 @@
+'use strict';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { useState } from 'react';
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
